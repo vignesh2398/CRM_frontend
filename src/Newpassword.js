34864b78e84066e928daa8c79f24f6cac2e5f2e4
Vignesh R <rvignesh60474@gmail.com>
2023-04-09 11:49:20 +0530

    09*04+2023

--- src/Newpassword.js
+++ src/Newpassword.js
@@ -30,7 +30,7 @@ export const Newpassword = () => {
           }
           else
           {
-              console.log("error")
+              console.log("errorrrrrr")
       
              
           }
